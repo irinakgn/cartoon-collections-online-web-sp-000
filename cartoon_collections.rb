2395d7ae5195f3e087f3bd6b9e7bbffe92685bc8
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 17:33:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,8 @@ def roll_call_dwarves(c)
 end
 
 def summon_captain_planet(c)
- c.map { |e| "#{e.titlecase()}!"  }
+ c.map { |e| "#{e.split(/\s+/).each{ |word| word.capitalize! }.join(' ') 
+}!"  }
 end
 
 def long_planeteer_calls(c)
